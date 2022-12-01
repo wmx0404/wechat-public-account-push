454efcc7cc5301909524f025a5383d1270e8d6ed
wmx0404 <85278759+wmx0404@users.noreply.github.com>
2022-12-01 15:38:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQNy46Mlw635vnDgu_ch2VEWCXsc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'N3v8d11aw7smJIL_qNlE81Dw3M0rdoEz5mT2CpMi3x0',
+      useTemplateId: '4IhoZHpRprWMs5h8w_jEDi5dpGgMAssNRCCjuz30uY4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-08',
       festivals: [
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '1996', date: '08-08',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-01',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -47,36 +47,6 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQNy46CqQV3LXa0lHh3S_diJeDFE',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OPooZ-8zz14fHmEQyZZjPrgjR4kLMGtpvzWj9SWZmLY',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '08-08',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    }
   ],
 
 
