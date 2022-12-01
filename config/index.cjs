00b673c64153bce2abb0ae55c8871746006d1551
wmx0404 <85278759+wmx0404@users.noreply.github.com>
2022-12-01 18:47:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'u4DSYSd_3aVC5PD1E8DPVuqP7eIMc_q19kJrrYG38L4',
+  CALLBACK_TEMPLATE_ID: '9ojB--R0iRf4lRH9bx-hTy5lTfivvmbghOPk3jvBG10',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '9ojB--R0iRf4lRH9bx-hTy5lTfivvmbghOPk3jvBG10',
+      id: 'oQNy46CqQV3LXa0lHh3S_diJeDFE',
     }
   ],
 
