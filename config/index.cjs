1b19194698bd62e50348d641d78f9205d866dae6
wmx0404 <85278759+wmx0404@users.noreply.github.com>
2022-12-01 17:12:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,28 +23,28 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQNy46Mlw635vnDgu_ch2VEWCXsc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'u4DSYSd_3aVC5PD1E8DPVuqP7eIMc_q19kJrrYG38L4',
+      useTemplateId: 'Zo-SIzfC-y_Y3RX4TQIqAgU-Q5dGQ4dOHZG05TVk67s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-08',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+//         {
+//           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+//         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '性感小鸣', year: '1996', date: '08-08',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '11-01',
-        },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2022', date: '11-01',
+//         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-01' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+//         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
