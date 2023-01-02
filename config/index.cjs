44e4697ac00e2bb78e9e4e29edf838e375c443d9
wmx0404 <85278759+wmx0404@users.noreply.github.com>
2023-01-02 23:14:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '鸣鸣宝贝',
+      name: '111',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQNy46BhqLfD1kJ3_trZDBnYvXHI',
+      id: 'oQNy46Mlw635vnDgu_ch2VEWCXsc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'f7HD334XmbWTNYTguXNiS07LboovWOhkM22CCck3dTg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
